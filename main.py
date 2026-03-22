fc05043aea1105009f4815a0165158a804b902a2
Shivanshi Sharma <neelusharma895764@gmail.com>
2026-03-22 09:43:02 +0530

    final deployment

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import pickle
 import pandas as pd
 
 app = FastAPI()
 
-model = pickle.load(open("model.pkl", "rb"))
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-class CarInput(BaseModel):
-    year: int
-    km_driven: int
-    fuel_type: int
-    seller_type: int
-    transmission: int
-    owner: int
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+    model_features = list(model.feature_names_in_)
 
 @app.get("/")
-def home():
-    return {"message": "Used Car Price Prediction API Running"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict")
-def predict(data: CarInput):
-
-    features = pd.DataFrame([{
-        "year": data.year,
-        "km_driven": data.km_driven,
-        "fuel_type": data.fuel_type,
-        "seller_type": data.seller_type,
-        "transmission": data.transmission,
-        "owner": data.owner
-    }])
-
-    prediction = model.predict(features)
-
-    return {
-        "predicted_price": round(float(prediction[0]), 2)
+def predict(
+    request: Request,
+    year: int = Form(...),
+    present_price: float = Form(...),
+    kms_driven: int = Form(...),
+    fuel_type: str = Form(...),
+    seller_type: str = Form(...),
+    transmission: str = Form(...),
+    owner: int = Form(...)
+):
+    input_data = {
+        'Year': year,
+        'Present_Price': present_price,
+        'Kms_Driven': kms_driven,
+        'Owner': owner,
+        'Fuel_Type_Diesel': 1 if fuel_type == "Diesel" else 0,
+        'Fuel_Type_Petrol': 1 if fuel_type == "Petrol" else 0,
+        'Seller_Type_Individual': 1 if seller_type == "Individual" else 0,
+        'Transmission_Manual': 1 if transmission == "Manual" else 0
     }
 
+    df = pd.DataFrame([input_data])
+    df = df.reindex(columns=model_features, fill_value=0)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    prediction = model.predict(df)[0]
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "prediction": round(float(prediction), 2)
+        }
+    )
